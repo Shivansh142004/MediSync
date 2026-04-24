e1167e389db82c3343042dc214bd70a8da1320c7
Shivansh Srivastav <150213911+Shivansh142004@users.noreply.github.com>
2026-04-24 08:55:39 +0530

    Update settings.py

--- Doctor/settings.py
+++ Doctor/settings.py
@@ -10,7 +10,9 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # ================= SECURITY =================
-SECRET_KEY = config('django-insecure-abc123xyz456SUPERSECRET')
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
